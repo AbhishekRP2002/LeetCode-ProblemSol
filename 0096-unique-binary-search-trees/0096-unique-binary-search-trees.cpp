96c80761332c53c8b628b79fa988836a40972dad
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2022-12-21 11:54:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.25%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int catalan_num(int n)
+    {
+        vector<int>cat(n+1);
+        cat[0] = cat[1] = 1;
+        for(int i=2;i<=n;++i)
+        {
+            cat[i] = 0;
+            for(int j = 0 ;j<i;++j)
+            {
+                cat[i] += cat[j]*cat[i-j-1];
+            }
+        }
+        
+        return cat[n];
+        
+    }
+    int numTrees(int n) {
+      int ans = catalan_num(n);
+        return ans;  
+    }
+};
