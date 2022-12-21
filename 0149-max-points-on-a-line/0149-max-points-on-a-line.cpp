f2f210fa2764ec784403be3c0c0b620957eb2ba4
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2022-12-21 13:09:21 +0530

    Time: 60 ms (68.87%), Space: 16.9 MB (38.44%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -23,13 +23,13 @@ public:
          int result = 0;
         for(int i=0; i<points.size();++i){
             unordered_map<long double,int> h;
-            int same = 1;
+            // int same = 1;
             int localmax = 0;
             for(int j=i+1;j<points.size();++j){
-                if(points[i][0] == points[j][0] && points[i][1]==points[j][1]){
-                    same++;
-                }
-                else if(points[i][0]==points[j][0]){
+                // if(points[i][0] == points[j][0] && points[i][1]==points[j][1]){
+                //     same++;
+                //}
+                 if(points[i][0]==points[j][0]){
                     h[INT_MAX]++;
                 }
                 else{
@@ -40,9 +40,9 @@ public:
             for(auto i : h){
                 localmax = max(localmax,i.second);
             }
-            localmax +=same;
+            // localmax +=same;
             result = max(result,localmax);
         }
-        return result;
+        return result+1;
     }
 };
