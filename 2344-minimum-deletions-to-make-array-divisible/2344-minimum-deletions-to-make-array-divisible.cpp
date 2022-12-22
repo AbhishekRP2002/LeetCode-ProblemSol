4b66961f566b5cd578daefa1b5f388ce6a43e68c
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2022-12-22 12:26:23 +0530

    Time: 167 ms (94.41%), Space: 88.2 MB (53.12%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.cpp
@@ -4,10 +4,10 @@ public:
         int ans = -1;
         int n = nums.size();
        sort(nums.begin() , nums.end());
-        map<int , int>m;
+        //map<int , int>m;
         int gcdd = *max_element(numsD.begin() , numsD.end());
-        for(int i=0;i<n;++i)
-            m[nums[i]] = i;
+        // for(int i=0;i<n;++i)
+        //     m[nums[i]] = i;
         for(auto i:numsD)
         {
             gcdd = __gcd(gcdd , i);
