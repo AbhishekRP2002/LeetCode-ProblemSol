09f8758cd3880eb54f8a25dfd23e56c3301e4afc
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2022-12-22 11:16:54 +0530

    Time: 305 ms (84.27%), Space: 90.9 MB (49.66%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+//     int dfs(int v  , int par ,  vector<int>adj[] , vector<int>subtree)
+//     {
+//          int curr = 0;
+//         for(auto u:adj[v]){
+//             if(u!=par){
+//                 curr+=dfs(u,v,adj,subtree);
+//                 curr+=subtree[u];
+//                 subtree[v]+=subtree[u];
+//             }
+//         }
+//         return curr;
+//     }
+    
+//      void dfs(int v,int par,vector<int> adj[],vector<int>& ans,vector<int>& subtree,int now){
+//         ans[v] = now;
+//         for(auto u:adj[v]){
+//             if(u!=par)
+//                 dfs(u,v,adj,ans,subtree,now-subtree[u]+subtree[0]-subtree[u]);
+//         }
+//     }
+    
+//     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+//         vector<int>sum(n) , adj[n] , subtree(n,1);
+//         for(auto i:edges)
+//         {
+//             adj[i[0]].push_back(i[1]);
+//             adj[i[1]].push_back(i[0]);
+            
+//         }
+        
+//         int root_ans = dfs( 0 , -1 ,adj , subtree );
+//          dfs( 0 , -1 ,adj , sum , subtree , root_ans );
+//         return sum;
+    
+     int dfs(int v,int par,vector<int> adj[],vector<int>& subtree){
+        int curr = 0;
+        for(auto u:adj[v]){
+            if(u!=par){
+                curr+=dfs(u,v,adj,subtree);
+                curr+=subtree[u];
+                subtree[v]+=subtree[u];
+            }
+        }
+        return curr;
+    }
+    void dfs(int v,int par,vector<int> adj[],vector<int>& ans,vector<int>& subtree,int now){
+        ans[v] = now;
+        for(auto u:adj[v]){
+            if(u!=par)
+                dfs(u,v,adj,ans,subtree,now-subtree[u]+subtree[0]-subtree[u]);
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int> ans(n),subtree(n,1),adj[n];
+        for(auto v:edges){
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        int root_ans = dfs(0,-1,adj,subtree);
+        dfs(0,-1,adj,ans,subtree,root_ans);
+        return ans;
+    }
+};
