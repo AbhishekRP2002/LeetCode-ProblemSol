7aa151237e58d4769574bbf7c56c4a10a4a553ba
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-01-01 16:26:36 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (19.87%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -5,10 +5,23 @@ public:
        int n = pattern.size();
         map<string , int>word2i; // MAPPING WORD TO INDEX AND PATTERN KA LETTER TO INDEX
         map<char , int>pattern2i;
+        string s2 = s;
         
         istringstream rd(s);
+        istringstream rd2(s2);
         
         int i= 0;
+        int ctr = 0;
+        string str ;
+        
+        while(rd2>>str)
+            ctr++;
+        
+        cout<<ctr<<" "<<n<<" "<<"\n";
+        
+        if(ctr != n)
+            return false;
+        
         for(string word ; rd>>word;i++)
         {
             if( pattern2i[pattern[i]]!= word2i[word])
