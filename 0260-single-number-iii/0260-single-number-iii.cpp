e54bdf3c5d7463012aac1d5e7f746cd42d61ab97
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-01-28 00:40:00 +0530

    Time: 16 ms (40.72%), Space: 11 MB (12.98%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-         sort(nums.begin() , nums.end());
+        
         
         int n = nums.size();
         
@@ -33,27 +33,48 @@ public:
         
         // Using HASH MAP we can solve this question
         //Using SET we can solve as well
-        // Using Sorting and single iteration for comparison we can also solve this question
+        set<int>st;
         vector<int>ans;
-        for(int i=0;i<n-1;)
+        st.insert(nums[0]);
+        for(int i=1;i<n;i++)
         {
-            if(nums[i]==nums[i+1])
+            if(st.count(nums[i]))
             {
-                i+=2;
+                st.erase(nums[i]);
+                continue;
             }
-            else
-            {
-                ans.push_back(nums[i]);
-                if(ans.size()==2)
-                    return ans;
-                i++;
-            }
+                    
+            st.insert(nums[i]);
+            
+            
         }
         
-        if(nums[n-1]!=nums[n-2])
-            ans.push_back(nums[n-1]);
+        for(auto i:st)
+            ans.push_back(i);
+        
         return ans;
-        // BIT MANIPULATION APPROACH 
+        // Using Sorting and single iteration for comparison we can also solve this question
+//         sort(nums.begin() , nums.end());
+//         vector<int>ans;
+//         for(int i=0;i<n-1;)
+//         {
+//             if(nums[i]==nums[i+1])
+//             {
+//                 i+=2;
+//             }
+//             else
+//             {
+//                 ans.push_back(nums[i]);
+//                 if(ans.size()==2)
+//                     return ans;
+//                 i++;
+//             }
+//         }
+        
+//         if(nums[n-1]!=nums[n-2])
+//             ans.push_back(nums[n-1]);
+//         return ans;
+//         BIT MANIPULATION APPROACH 
         
         
         
