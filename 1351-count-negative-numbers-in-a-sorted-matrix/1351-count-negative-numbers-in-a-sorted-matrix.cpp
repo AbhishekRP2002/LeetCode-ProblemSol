b35638a01ec9aa6a7578f89af35fdd5fb82ff084
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-02-10 15:28:09 +0530

    Time: 11 ms (97.39%), Space: 11.5 MB (6.27%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,19 +1,57 @@
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size() , cnt=0;
-        
-        for(int i=0;i<m;++i)
+    void countNeg(vector<int>nums , int& cnt)
+    {
+        int i = 0 , j= nums.size()-1;
+        while(j-i>1)
         {
-            for(int j=0;j<n;++j)
+            int mid = i + (j-i)/2;
+            if(nums[mid] < 0)
             {
-                if(grid[i][j] < 0)
-                    cnt++;
+                i = mid;
+            }
+            else
+            {
+                j=mid-1;
             }
         }
         
+        if(nums[j] < 0)
+            cnt+= j+1;
+        else if(nums[i] < 0)
+            cnt+=i+1;
+        else cnt=0;
+    }
+    int countNegatives(vector<vector<int>>& grid) {
+//Brute Force Approach
+        //         int m = grid.size();
+//         int n = grid[0].size() , cnt=0;
+        
+//         for(int i=0;i<m;++i)
+//         {
+//             for(int j=0;j<n;++j)
+//             {
+//                 if(grid[i][j] < 0)
+//                     cnt++;
+//             }
+//         }
+        
+        
+//         return cnt;
+        
+        // Binary Search Approach
+        
+    int n = grid.size();
+    int cnt = 0 ;
+    
+    for(int i=0;i<n;++i)
+    {
+        reverse(grid[i].begin() , grid[i].end());
+        vector<int>v(grid[i]);
+        countNeg(v,cnt);
+    }
         
         return cnt;
+    
     }
 };
