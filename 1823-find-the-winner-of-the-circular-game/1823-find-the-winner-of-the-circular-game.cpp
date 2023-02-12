37a64971a91f1c445c0e6057660611ae6ab1d583
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-02-12 10:25:02 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.71%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -9,9 +9,14 @@ public:
     }
     int findTheWinner(int n, int k) {
         
-       return util(n,k)+1;
+//        return util(n,k)+1;
+        int ans  = 0;
+        for(int i=1;i<=n;++i)
+        {
+            ans = (ans + k)%i;
+        }
         
-        
-        
+        return ans+1;
+       
     }
 };
