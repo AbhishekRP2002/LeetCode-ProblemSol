610414c151ddeee78a84840054bb193010b16b54
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-03-03 22:44:00 +0530

    Time: 9 ms (19.71%), Space: 9 MB (31.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,7 +9,28 @@ public:
         //     }
         // }
         // return nums.size()-1;
-        return max_element(nums.begin() , nums.end()) - nums.begin();
+        // return max_element(nums.begin() , nums.end()) - nums.begin();
+//         binary search approach
+        int n = nums.size();
+        if(n==1)
+            return 0;
+        if(n==2)
+            return nums[0]>nums[1] ? 0:1;
+        int low = 0 ,high =  n-1;
+        while(high - low > 1)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1])
+                return mid;
+            if(nums[mid] < nums[mid+1])
+                low = mid+1;
+            else
+                high = mid;
+        }
     
+        if(nums[high] > nums[low])
+            return high;
+        return low;
+        
     }
 };
