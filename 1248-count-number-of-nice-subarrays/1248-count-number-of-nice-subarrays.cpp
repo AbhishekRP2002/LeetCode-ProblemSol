ffa68232a43d01707e43057fcf38bbfa257cf235
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-03-04 18:24:22 +0530

    Time: 137 ms (85.54%), Space: 67.7 MB (80.09%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+//         Variable length sliding window 
+//     BF- Find all subarrays and no of odd nums in them , check with k 
+//         int i =0, j= 0,cnt=0, ans = 0, temp =0;
+//         while(j<n)
+//         {
+//             if(nums[j]&1)
+//                 cnt++;
+//             if(cnt < k)
+//             {
+//                 j++;
+//             }
+//             else
+//             {
+//                 while(cnt==k)
+//                 {
+                                       
+//                     temp++;
+//                     cnt-= nums[i]&1;
+//                     i++;
+//                 } 
+                
+//            ans+=temp;
+//                  temp =0;
+                
+//                     j++;
+//                 }
+//             }
+            
+        
+        
+        
+//         return ans;
+;
+        int ans = 0,odd = 0,cnt = 0;
+        int l = 0,r = 0;
+        while(r<n)
+        {
+            if(nums[r]%2 != 0)
+            {
+                odd++;
+                cnt = 0;
+            }
+            while(odd == k)
+            {
+                ++cnt;
+                odd -= nums[l++]&1; 
+            }
+            ans += cnt;
+            r++;
+        }
+        return ans;
+    }
+};
