ac88de6dffe4111fb4fd50e184e418d2ba0316e8
Abhishek Ranjan <86261428+AbhishekRP2002@users.noreply.github.com>
2023-03-11 22:58:28 +0530

    Time: 3 ms (82.69%), Space: 12.7 MB (36.24%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        // Approach -1 
+//         queue<TreeNode*>q;
+//         TreeNode* temp ;
+//         vector<vector<int>>res;
+//         if(root)
+//         {
+//             q.push(root);
+//             q.push(NULL);
+//             vector<int>level;
+//             while(!q.empty())
+//             {
+//                  temp = q.front();
+//                  q.pop();
+//                 if(temp==NULL)
+//                 {
+//                     res.push_back(level);
+//                     level.clear();
+//                     if(!q.empty()) q.push(NULL);
+//                 }
+//                 else
+//                 {
+//                     level.push_back(temp->val);
+//                     if(temp->left) q.push(temp->left);
+//                      if(temp->right) q.push(temp->right);
+//                 }
+//             }
+           
+            
+//         }
+//         return res;
+//         Approaach -2 
+        
+        queue<TreeNode*>q;
+        vector<vector<int>>ans;
+        
+        if(root)
+        {
+            q.push(root);
+            while(!q.empty())
+            {
+                int n = q.size();
+                vector<int>level;
+                for(int i=0;i<n;++i)
+                {
+                    
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+                level.push_back(temp->val);
+                }
+                
+                ans.push_back(level);
+            
+            }
+            
+            
+            
+        }
+        
+        
+        return ans;
+        
+    }
+};
